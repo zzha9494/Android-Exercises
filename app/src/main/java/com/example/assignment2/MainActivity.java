3e167e1b9843c6cc6c65711177100b1c5d76bb85
ZJ <zzha9494@uni.sydney.edu.au>
2022-09-25 13:56:13 +1000

    start http request

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.assignment2;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +17,6 @@ import android.widget.Toast;
 import android.widget.VideoView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
@@ -27,10 +26,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int MY_REQUEST_OPEN_CAMERA = 101;
 
-    VideoView mVideoView ;
+    VideoView mVideoView;
     ImageView mImageView;
     TextView mTextView;
 
@@ -50,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
     // Keys for storing activity state.
     private static final String KEY_LOCATION = "location";
+
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onTakePhotoClick(View v) {
-        getDeviceLocation();
         if (!getPermission())
             return;
 
@@ -90,18 +88,20 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, MY_REQUEST_OPEN_CAMERA);
     }
 
+    @SuppressLint("MissingSuperCall")
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == MY_REQUEST_OPEN_CAMERA) {
-            if (resultCode == RESULT_OK)
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Successful", Toast.LENGTH_SHORT).show();
-            else
+                getDeviceLocation();
+            } else
                 Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
         }
     }
 
     private boolean getPermission() {
-        String[] permissions = new String[] {
+        String[] permissions = new String[]{
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA,
@@ -109,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_FINE_LOCATION
         };
 
-        for (String permission: permissions) {
+        for (String permission : permissions) {
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(), permission)
                     != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
@@ -123,14 +123,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Uri getFileUri(String type) {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        File f = new File(getExternalFilesDir(null).toString(), timeStamp + type);
+        fileName = timeStamp + type;
+        File f = new File(getExternalFilesDir(null).toString(), fileName);
 
         Uri fileUri;
         if (Build.VERSION.SDK_INT >= 24) {
             fileUri = FileProvider.getUriForFile(this.getApplicationContext(),
                     "com.example.assignment2.fileProvider", f);
-        }
-        else {
+        } else {
             fileUri = Uri.fromFile(f);
         }
 
@@ -142,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
         if (!getPermission())
             return;
 
+        @SuppressLint("MissingPermission")
         Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
         locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
             @Override
